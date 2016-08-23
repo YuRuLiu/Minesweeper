c98e7eae62f8f4e6f6c573b21696901d2b19d423
betsy_liu <40143204@gm.nfu.edu.tw>
2016-08-23 09:55:12 +0000

    MinesweeperVer.php:修改輸出,讓輸出趨近解答

--- MinesweeperVer.php
+++ MinesweeperVer.php
@@ -1,88 +1,99 @@
 <?php
 header("Content-Type:text/html; charset=utf-8");
-$y="";
-$map = $_GET['map'];
+
+$map = substr($_SERVER['QUERY_STRING'], 4);
+
+$maplen = strlen($map);
+if ($maplen != 109) {
+    echo "不符合，因為字串長度為".$maplen."，正確長度應為109個字元。";
+    $false = 1;
+}
+
+if (preg_match("/m/", $map)) {
+    echo "地雷大小寫有錯，m須為大寫。";
+    $false = 1;
+}
+
+$countM = substr_count($map, "M");
+$countm = substr_count($map, "m");
+$totalM = $countM + $countm;
+if ($totalM != 40) {
+    echo "地雷數量有錯，地雷數量為".$totalM."顆，正確應為40顆。";
+    $false = 1;
+}
+
+if (preg_match("/n/", $map)) {
+    echo "不符合，N須為大寫。";
+    $false = 1;
+}
 
 $countN = substr_count($map, "N");
+if ($countN != 9) {
+    echo "斷行次數錯誤，斷行次數為".$countN."，正確應為9。";
+    $false = 1;
+}
 
 $mapWithoutN = str_replace( "N" , "" , $map);
 $mapArray =  preg_split('//', $mapWithoutN, -1, PREG_SPLIT_NO_EMPTY);
 
-$maplen = strlen($map);
-$mapWithoutNLen = strlen($mapWithoutN);
+for ($x=0;$x<100;$x++) {
+    $count[] = "0";
+}
 
-if ($maplen == 109) {
-    for ($x=0;$x<100;$x++) {
-        $count[] = "0";
+for ($i=0;$i<100;$i++) {
+
+    if ($mapArray[$i] == "M" || $mapArray[$i] == "m") {
+        $count[$i] = "M";
     }
 
-    for ($i=0;$i<100;$i++) {
-        if ($mapArray[$i] == "M" ) {
-            $countM ++;
-            $count[$i] = "M";
+    if(is_numeric($mapArray[$i])) {
+        //左
+        if (($mapArray[$i-1] == "M" || $mapArray[$i-1] == "m") && (($i-1)%10 != 9)) {
+            $count[$i]++;
         }
-
-        if(is_numeric($mapArray[$i])) {
-            //左
-            if ($mapArray[$i-1] == "M" && (($i-1)%10 != 9)) {
-                $count[$i]++;
-            }
-            //右
-            if ($mapArray[$i+1] == "M" && (($i+1)%10 != 0)) {
-                $count[$i]++;
-            }
-            //上
-            if ($mapArray[$i-10] == "M") {
-                $count[$i]++;
-            }
-            //下
-            if ($mapArray[$i+10] == "M") {
-                $count[$i]++;
-            }
-            //左上
-            if ($mapArray[$i-11] == "M" && (($i-11)%10 != 9)) {
-                $count[$i]++;
-            }
-            //右下
-            if ($mapArray[$i+11] == "M" && (($i+11)%10 != 0)) {
-                $count[$i]++;
-            }
-            //右上
-            if ($mapArray[$i-9] == "M" && (($i-9)%10 != 0)) {
-                $count[$i]++;
-            }
-            //左下
-            if ($mapArray[$i+9] == "M" && (($i+9)%10 != 9)) {
-                $count[$i]++;
-            }
+        //右
+        if (($mapArray[$i+1] == "M" || $mapArray[$i+1] == "m") && (($i+1)%10 != 0)) {
+            $count[$i]++;
         }
-
-        if ($mapArray[$i] == $count[$i]) {
-            $true = 1;
+        //上
+        if ($mapArray[$i-10] == "M" || $mapArray[$i-10] == "m") {
+            $count[$i]++;
         }
-        if ($mapArray[$i] != $count[$i]) {
-            echo "不符合，因為數字錯。位置=".$i."正確應為".$count[$i]."<br>";
-            $false = 1;
+        //下
+        if ($mapArray[$i+10] == "M" || $mapArray[$i+10] == "m") {
+            $count[$i]++;
+        }
+        //左上
+        if (($mapArray[$i-11] == "M" || $mapArray[$i-11] == "m") && (($i-11)%10 != 9)) {
+            $count[$i]++;
+        }
+        //右下
+        if (($mapArray[$i+11] == "M" || $mapArray[$i+11] == "m") && (($i+11)%10 != 0)) {
+            $count[$i]++;
+        }
+        //右上
+        if (($mapArray[$i-9] == "M" || $mapArray[$i-9] == "m") && (($i-9)%10 != 0)) {
+            $count[$i]++;
+        }
+        //左下
+        if (($mapArray[$i+9] == "M" || $mapArray[$i+9] == "m") && (($i+9)%10 != 9)) {
+            $count[$i]++;
         }
     }
 
-    if ($countN != 9) {
-        echo "不符合，因為N的數量有誤";
+    if ($mapArray[$i] == $count[$i]) {
+        $true = 1;
+    }
+    if ($mapArray[$i] != $count[$i]) {
+        echo "數字對應有錯。位置".$i."：".$mapArray[$i]."，正確應為".$count[$i]."。";
         $false = 1;
     }
+}
 
-    if ($countM != 40) {
-        echo "不符合，因為M的數量有誤";
-        $false = 1;
-    }
+if ($false) {
+    exit;
+}
 
-    if ($false) {
-        exit;
-    }
-
-    if ($true) {
-        echo "符合。";
-    }
-} else {
-    echo "不符合，因為字串數量有誤";
+if ($true && !$false) {
+    echo "正確的。";
 }
